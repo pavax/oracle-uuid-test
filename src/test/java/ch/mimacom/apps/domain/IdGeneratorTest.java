ae2c66e43d499f0800d812fb9207bb3289a8883e
Patrick Dobler <patrick.dobler@gmail.com>
2014-08-02 10:06:51 +0200

    small tests

--- src/test/java/ch/mimacom/apps/domain/IdGeneratorTest.java
+++ src/test/java/ch/mimacom/apps/domain/IdGeneratorTest.java
@@ -1,12 +1,27 @@
 package ch.mimacom.apps.domain;
 
+import com.fasterxml.uuid.EthernetAddress;
+import org.junit.Test;
+
 import java.util.UUID;
 
 public class IdGeneratorTest {
 
-    @org.junit.Test
+    @Test
     public void testGenerateId() throws Exception {
-        UUID uuid = IdGenerator.timeBasedUUID();
-        System.out.println(uuid);
+        final EthernetAddress ETHERNET_ADDRESS = EthernetAddress.fromInterface();
+        for (int i = 0; i < 5; i++) {
+            System.out.println(IdGenerator.timeBasedUUID());
+        }
+    }
+
+    @Test
+    public void testManualUUID() throws Exception {
+        System.out.println(UUID.fromString("00000000-0000-0000-0000-000000000001"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testManualUUIDInvalid() throws Exception {
+        System.out.println(UUID.fromString("0000-0000-0000-0000"));
     }
 }
